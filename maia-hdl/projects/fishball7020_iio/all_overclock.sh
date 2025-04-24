7f5bf9b6139d7237b0121758314601d851784858
F5OEO <evaristec@gmail.com>
2025-04-24 16:24:12 +0200

    Add fishball 7020

---
+++ maia-hdl/projects/fishball7020_iio/all_overclock.sh
@@ -0,0 +1,6 @@
+#Standard
+./overclock.sh 40 32
+./overclock.sh 58 36
+./overclock.sh 64 36
+#Need 11 8 11 11
+./overclock.sh 62 48
---
+++ maia-hdl/projects/fishball7020_iio/system_project.tcl
@@ -0,0 +1,20 @@
+source ../../adi-hdl/scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+set p_device "xc7z020clg400-1"
+adi_project fishball
+
+adi_project_files fishball [list \
+  "../fishball_iio/system_top.v" \
+  "../fishball_iio/system_constr.xdc" \
+  "$ad_hdl_dir/library/common/ad_iobuf.v"]
+
+
+# use improved implementation strategy for best timing results
+set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
+
+set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
+adi_project_run fishball
+source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
+
