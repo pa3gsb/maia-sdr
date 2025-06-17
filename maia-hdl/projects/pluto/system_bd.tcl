9fed007d9bbe6d3ccf1465a625f8f77bf393f2ec
F5OEO <evaristec@gmail.com>
2025-06-17 20:02:50 +0200

    Working Decim with maia

--- maia-hdl/projects/pluto/system_bd.tcl
+++ maia-hdl/projects/pluto/system_bd.tcl
@@ -631,72 +631,9 @@ if {[info exists maia_iio]} {
 	ad_connect axi_ad9361/adc_enable_i0 cpack/enable_0
 	#ad_connect axi_ad9361/adc_data_q0 cpack/fifo_wr_data_1
 
-#if {[info exists fifo_xilinx]} {
-ad_ip_instance axis_data_fifo interclk_i
-ad_ip_parameter interclk_i CONFIG.FIFO_DEPTH 16
-ad_ip_parameter interclk_i CONFIG.FIFO_MODE 1
-ad_ip_parameter interclk_i CONFIG.IS_ACLK_ASYNC 1
-ad_ip_parameter interclk_i CONFIG.HAS_TLAST.VALUE_SRC USER
-ad_ip_parameter interclk_i CONFIG.HAS_TLAST 0
-ad_ip_parameter interclk_i CONFIG.TDATA_NUM_BYTES 2
-ad_ip_parameter interclk_i CONFIG.SYNCHRONIZATION_STAGES 4 
-
-ad_connect interclk_i/m_axis_aclk maia_sdr_clk/clk_out1
-ad_connect sys_cpu_resetn interclk_i/s_axis_aresetn 
-ad_connect  axi_ad9361/adc_data_i0 interclk_i/s_axis_tdata
-ad_connect  axi_ad9361/l_clk interclk_i/s_axis_aclk
-ad_connect  axi_ad9361/adc_valid_i0 interclk_i/s_axis_tvalid
-ad_connect interclk_i/m_axis_tdata adc_i_slice/Din
-ad_connect interclk_i/m_axis_tvalid maia_sdr/sampling_clk
-
-d_ip_instance axis_data_fifo interclk_q
-ad_ip_parameter interclk_q CONFIG.FIFO_DEPTH 16
-ad_ip_parameter interclk_q CONFIG.FIFO_MODE 1
-ad_ip_parameter interclk_q CONFIG.IS_ACLK_ASYNC 1
-ad_ip_parameter interclk_q CONFIG.HAS_TLAST.VALUE_SRC USER
-ad_ip_parameter interclk_q CONFIG.HAS_TLAST 0
-ad_ip_parameter interclk_q CONFIG.TDATA_NUM_BYTES 2
-ad_ip_parameter interclk_q CONFIG.SYNCHRONIZATION_STAGES 4 
-
-ad_connect interclk_q/m_axis_aclk maia_sdr_clk/clk_out1
-ad_connect sys_cpu_resetn interclk_q/s_axis_aresetn 
-ad_connect  axi_ad9361/adc_data_q0 interclk_q/s_axis_tdata
-ad_connect  axi_ad9361/l_clk interclk_q/s_axis_aclk
-ad_connect  axi_ad9361/adc_valid_q0 interclk_q/s_axis_tvalid
-ad_connect interclk_q/m_axis_tdata adc_q_slice/Din
-#ad_connect interclk_q/m_axis_tvalid maia_sdr/sampling_clk
-
-#}
-if {[info exists fifo_ad]} {
-ad_ip_instance util_axis_fifo interclk_i
-ad_ip_parameter interclk_i CONFIG.DATA_WIDTH 16
-
-ad_connect interclk_i/m_axis_aclk maia_sdr_clk/clk_out1
-ad_connect sys_cpu_resetn interclk_i/s_axis_aresetn 
-ad_connect sys_cpu_resetn interclk_i/m_axis_aresetn 
-ad_connect  axi_ad9361/adc_data_i0 interclk_i/s_axis_data
-ad_connect  axi_ad9361/l_clk interclk_i/s_axis_aclk
-ad_connect  axi_ad9361/adc_valid_i0 interclk_i/s_axis_valid
-ad_connect interclk_i/m_axis_data adc_i_slice/Din
-ad_connect interclk_i/m_axis_valid maia_sdr/sampling_clk
-
-ad_ip_instance util_axis_fifo interclk_q
-ad_ip_parameter interclk_q CONFIG.DATA_WIDTH 16
-
-ad_connect interclk_q/m_axis_aclk maia_sdr_clk/clk_out1
-ad_connect sys_cpu_resetn interclk_q/s_axis_aresetn 
-ad_connect sys_cpu_resetn interclk_q/m_axis_aresetn 
-ad_connect  axi_ad9361/adc_data_q0 interclk_q/s_axis_data
-ad_connect  axi_ad9361/l_clk interclk_q/s_axis_aclk
-ad_connect  axi_ad9361/adc_valid_q0 interclk_q/s_axis_valid
-ad_connect interclk_q/m_axis_data adc_q_slice/Din
-#ad_connect interclk_q/m_axis_valid maia_sdr/sampling_clk
-
-}
-
-
-
-
+ad_connect axi_ad9361/adc_data_i0 adc_i_slice/Din
+ad_connect axi_ad9361/adc_data_q0 adc_q_slice/Din
+ad_connect axi_ad9361/adc_valid_i0 maia_sdr/sampling_clk
 
 
 	ad_connect axi_ad9361/l_clk cpack/clk
@@ -1106,26 +1043,63 @@ if {[info exists with_rx_fir_maia]} {
 
 	
 #I	
-	create_bd_cell -type module -reference ad_bus_mux mux_decim_i
+ad_ip_instance axis_data_fifo interclk_i
+create_bd_cell -type module -reference ad_bus_mux mux_decim_i
+
+ad_ip_parameter interclk_i CONFIG.FIFO_DEPTH 16
+ad_ip_parameter interclk_i CONFIG.FIFO_MODE 1
+ad_ip_parameter interclk_i CONFIG.IS_ACLK_ASYNC 1
+ad_ip_parameter interclk_i CONFIG.HAS_TLAST.VALUE_SRC USER
+ad_ip_parameter interclk_i CONFIG.HAS_TLAST 0
+ad_ip_parameter interclk_i CONFIG.TDATA_NUM_BYTES 2
+ad_ip_parameter interclk_i CONFIG.SYNCHRONIZATION_STAGES 4 
+
+ad_connect interclk_i/m_axis_aclk  axi_ad9361/l_clk 
+ad_connect sys_cpu_resetn interclk_i/s_axis_aresetn 
+ad_connect  maia_sdr/decim_re_out interclk_i/s_axis_tdata
+ad_connect  maia_sdr_clk/clk_out1 interclk_i/s_axis_aclk
+ad_connect  maia_sdr/decim_strobe_out interclk_i/s_axis_tvalid
+ad_connect interclk_i/m_axis_tdata mux_decim_i/data_in_1
+ad_connect interclk_i/m_axis_tvalid mux_decim_i/valid_in_1
+
+
+	
 	ad_connect mux_decim_i/select_path decim_slice/Dout
 	ad_connect axi_ad9361/adc_valid_i0 mux_decim_i/valid_in_0
 	ad_connect axi_ad9361/adc_enable_i0 mux_decim_i/enable_in_0
 	ad_connect axi_ad9361/adc_data_i0 mux_decim_i/data_in_0
-	ad_connect maia_sdr/decim_strobe_out mux_decim_i/valid_in_1
+	#ad_connect maia_sdr/decim_strobe_out mux_decim_i/valid_in_1
 	ad_connect axi_ad9361/adc_enable_i0 mux_decim_i/enable_in_1
-	ad_connect maia_sdr/decim_re_out mux_decim_i/data_in_1
+	#ad_connect maia_sdr/decim_re_out mux_decim_i/data_in_1
 
 #Q	
+ad_ip_instance axis_data_fifo interclk_q
+create_bd_cell -type module -reference ad_bus_mux mux_decim_q
 
-	create_bd_cell -type module -reference ad_bus_mux mux_decim_q
+ad_ip_parameter interclk_q CONFIG.FIFO_DEPTH 16
+ad_ip_parameter interclk_q CONFIG.FIFO_MODE 1
+ad_ip_parameter interclk_q CONFIG.IS_ACLK_ASYNC 1
+ad_ip_parameter interclk_q CONFIG.HAS_TLAST.VALUE_SRC USER
+ad_ip_parameter interclk_q CONFIG.HAS_TLAST 0
+ad_ip_parameter interclk_q CONFIG.TDATA_NUM_BYTES 2
+ad_ip_parameter interclk_q CONFIG.SYNCHRONIZATION_STAGES 4 
+
+ad_connect interclk_q/m_axis_aclk  axi_ad9361/l_clk 
+ad_connect sys_cpu_resetn interclk_q/s_axis_aresetn 
+ad_connect  maia_sdr/decim_im_out interclk_q/s_axis_tdata
+ad_connect  maia_sdr_clk/clk_out1 interclk_q/s_axis_aclk
+ad_connect  maia_sdr/decim_strobe_out interclk_q/s_axis_tvalid
+ad_connect interclk_q/m_axis_tdata mux_decim_q/data_in_1
+ad_connect interclk_q/m_axis_tvalid mux_decim_q/valid_in_1
+
 	ad_connect mux_decim_q/select_path decim_slice/Dout
 	ad_connect axi_ad9361/adc_valid_q0 mux_decim_q/valid_in_0
 	ad_connect axi_ad9361/adc_enable_q0 mux_decim_q/enable_in_0
 	ad_connect axi_ad9361/adc_data_q0 mux_decim_q/data_in_0
 
-	ad_connect maia_sdr/decim_strobe_out mux_decim_q/valid_in_1
+	#ad_connect maia_sdr/decim_strobe_out mux_decim_q/valid_in_1
 	ad_connect axi_ad9361/adc_enable_q0 mux_decim_q/enable_in_1
-	ad_connect maia_sdr/decim_im_out mux_decim_q/data_in_1
+	#ad_connect maia_sdr/decim_im_out mux_decim_q/data_in_1
 
 	ad_connect mux_decim_i/data_out muxcs8/data_in_0
 	ad_connect mux_decim_i/valid_out muxcs8/valid_in_0
