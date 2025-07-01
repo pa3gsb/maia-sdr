1c9015064639f11e082771c0b6f20ce3a6329064
F5OEO <evaristec@gmail.com>
2025-07-01 10:39:53 +0200

    Fix fir maia for LibreSDR

--- maia-hdl/projects/libre_iio/system_bd.tcl
+++ maia-hdl/projects/libre_iio/system_bd.tcl
@@ -2,5 +2,6 @@
 set libre "libre"
 set maia_iio "maia_iio"
 set with_tx_fir "with_tx_fir"
-set with_rx_fir "with_rx_fir"
+#set with_rx_fir "with_rx_fir"
+set with_rx_fir_maia "with_rx_fir_maia"
 source ../pluto/system_bd.tcl
