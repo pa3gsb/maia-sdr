8505e84edc0fdb54f1cfbd201aab52a4960a4fb2
F5OEO <evaristec@gmail.com>
2026-01-06 16:57:19 +0100

    Sweeper mispell

--- maia-hdl/projects/common/sweeper.tcl
+++ maia-hdl/projects/common/sweeper.tcl
@@ -1,13 +1,17 @@
 ###### SWEEPER ###########
-	add_files -norecurse  ../common/sweeper_it.v
+	ad_disconnect  gpio_o sys_ps7/GPIO_O	
+
+
 	create_bd_cell -type module -reference sweeper_it sweeper_io
 	ad_connect sweeper_io/clk maia_sdr/clk_fastlock_out
-	#ad_connect sweeper_io/clk axi_ad9361/l_clk
 	ad_connect sweeper_io/reset axi_ad9361/rst
 	ad_connect sweeper_io/profile_o maia_sdr/fastlock_profile_in
 	ad_ip_instance util_vector_logic logic_orgpio [list \
 	  C_OPERATION {or} \
 	  C_SIZE 64 ]
-	ad_connect logic_orgpio/Res gpio_o
+
+	create_bd_port -dir O -from 63 -to 0 gpio_o
+	
 	ad_connect sweeper_io/gpio_o logic_orgpio/Op1
 	ad_connect sys_ps7/GPIO_O logic_orgpio/Op2
+	ad_connect logic_orgpio/Res gpio_o
