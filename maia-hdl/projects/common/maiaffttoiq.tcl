485d8abc1d6812f031fb1463bda10c99da79ee80
F5OEO <evaristec@gmail.com>
2026-01-07 11:11:09 +0100

    Use same technic as fir to output fft raw samples to standard i/q

---
+++ maia-hdl/projects/common/maiaffttoiq.tcl
@@ -0,0 +1,77 @@
+#disconnect existing
+ad_disconnect util_ad9361_adc_fifo/dout_data_0 util_ad9361_adc_pack/fifo_wr_data_0
+ad_disconnect util_ad9361_adc_fifo/dout_data_1 util_ad9361_adc_pack/fifo_wr_data_1
+ad_disconnect util_ad9361_adc_pack/fifo_wr_en util_ad9361_adc_fifo/dout_valid_0
+ad_disconnect util_ad9361_adc_fifo/dout_enable_0 util_ad9361_adc_pack/enable_0
+ad_disconnect util_ad9361_adc_fifo/dout_enable_1 util_ad9361_adc_pack/enable_1
+
+
+#In order to be controled by iio		
+ad_ip_instance xlslice decim_slice
+ad_connect axi_ad9361/up_adc_gpio_out decim_slice/Din
+	
+#I	
+ad_ip_instance axis_data_fifo interclk_i
+create_bd_cell -type module -reference ad_bus_mux mux_decim_i
+
+ad_ip_parameter interclk_i CONFIG.FIFO_DEPTH 16
+ad_ip_parameter interclk_i CONFIG.FIFO_MODE 1
+ad_ip_parameter interclk_i CONFIG.IS_ACLK_ASYNC 1
+ad_ip_parameter interclk_i CONFIG.HAS_TLAST.VALUE_SRC USER
+ad_ip_parameter interclk_i CONFIG.HAS_TLAST 0
+ad_ip_parameter interclk_i CONFIG.TDATA_NUM_BYTES 2
+ad_ip_parameter interclk_i CONFIG.SYNCHRONIZATION_STAGES 4 
+
+ad_connect interclk_i/m_axis_aclk  util_ad9361_divclk/clk_out
+ad_connect sys_cpu_resetn interclk_i/s_axis_aresetn 
+ad_connect  maia_sdr/fft_re_out interclk_i/s_axis_tdata
+ad_connect  maia_sdr_clk/clk_out1 interclk_i/s_axis_aclk
+ad_connect  maia_sdr/fft_strobe_out interclk_i/s_axis_tvalid
+ad_connect interclk_i/m_axis_tdata mux_decim_i/data_in_1
+ad_connect interclk_i/m_axis_tvalid mux_decim_i/valid_in_1
+
+
+ad_connect mux_decim_i/select_path decim_slice/Dout
+ad_connect util_ad9361_adc_fifo/dout_valid_0 mux_decim_i/valid_in_0
+ad_connect util_ad9361_adc_fifo/dout_enable_0 mux_decim_i/enable_in_0
+ad_connect util_ad9361_adc_fifo/dout_data_0 mux_decim_i/data_in_0
+ad_connect util_ad9361_adc_fifo/dout_enable_0 mux_decim_i/enable_in_1
+
+#Q	
+ad_ip_instance axis_data_fifo interclk_q
+create_bd_cell -type module -reference ad_bus_mux mux_decim_q
+
+ad_ip_parameter interclk_q CONFIG.FIFO_DEPTH 16
+ad_ip_parameter interclk_q CONFIG.FIFO_MODE 1
+ad_ip_parameter interclk_q CONFIG.IS_ACLK_ASYNC 1
+ad_ip_parameter interclk_q CONFIG.HAS_TLAST.VALUE_SRC USER
+ad_ip_parameter interclk_q CONFIG.HAS_TLAST 0
+ad_ip_parameter interclk_q CONFIG.TDATA_NUM_BYTES 2
+ad_ip_parameter interclk_q CONFIG.SYNCHRONIZATION_STAGES 4 
+
+ad_connect interclk_q/m_axis_aclk  util_ad9361_divclk/clk_out
+ad_connect sys_cpu_resetn interclk_q/s_axis_aresetn 
+ad_connect  maia_sdr/fft_im_out interclk_q/s_axis_tdata
+ad_connect  maia_sdr_clk/clk_out1 interclk_q/s_axis_aclk
+ad_connect  maia_sdr/fft_strobe_out interclk_q/s_axis_tvalid
+ad_connect interclk_q/m_axis_tdata mux_decim_q/data_in_1
+ad_connect interclk_q/m_axis_tvalid mux_decim_q/valid_in_1
+
+ad_connect mux_decim_q/select_path decim_slice/Dout
+ad_connect util_ad9361_adc_fifo/dout_valid_1 mux_decim_q/valid_in_0
+ad_connect util_ad9361_adc_fifo/dout_enable_1 mux_decim_q/enable_in_0
+ad_connect util_ad9361_adc_fifo/dout_data_1 mux_decim_q/data_in_0
+ad_connect util_ad9361_adc_fifo/dout_enable_1 mux_decim_q/enable_in_1
+
+
+
+
+ad_connect mux_decim_i/data_out util_ad9361_adc_pack/fifo_wr_data_0
+ad_connect mux_decim_q/data_out util_ad9361_adc_pack/fifo_wr_data_1
+ad_connect mux_decim_i/valid_out util_ad9361_adc_pack/fifo_wr_en
+ad_connect mux_decim_i/enable_out util_ad9361_adc_pack/enable_0
+ad_connect mux_decim_q/enable_out util_ad9361_adc_pack/enable_1
+
+#SEEMS THE DISCONNECT HAVE DELETE ALSO EXISTING
+ad_connect util_ad9361_adc_fifo/dout_data_0 adc_i_slice/Din
+ad_connect util_ad9361_adc_fifo/dout_data_1 adc_q_slice/Din
