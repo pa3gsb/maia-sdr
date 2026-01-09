b2f7d8b50ff39326599c2a6b3666c647174fccc9
F5OEO <evaristec@gmail.com>
2026-01-09 10:40:13 +0100

    Revert back maia-wasm and disable double clk : will do in overlay instead

--- maia-wasm/assets/index.js
+++ maia-wasm/assets/index.js
@@ -6,10 +6,3 @@ async function run() {
 };
 
 run();
-
-const canvas = document.getElementById("canvas");
-const panel  = document.querySelector("form.ui"); // main control panel
-
-canvas.addEventListener("dblclick", () => {
-  panel.classList.toggle("hidden");
-});
