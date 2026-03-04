d25831ac36b384f7017662e6521f6c2299fbeb25
F5OEO <evaristec@gmail.com>
2026-03-04 10:42:58 +0100

    Update port for 7020 daughter board

--- maia-hdl/projects/boards/fishball7020/ports.tcl
+++ maia-hdl/projects/boards/fishball7020/ports.tcl
@@ -5,4 +5,5 @@ create_bd_port -dir O phase
 create_bd_port -dir I serial_rx
 create_bd_port -dir O serial_tx
 create_bd_port -dir O lock
-create_bd_port -dir O clk_100khz
+create_bd_port -dir O -type clk outclk
+
